01bee67d9c6d056ef8cf1ff44d809ac10a7a5e64
mh <mh@immerda.ch>
2011-08-08 00:20:52 +0200

    wording

--- lib/trocla/formats/pgsql.rb
+++ lib/trocla/formats/pgsql.rb
@@ -1,7 +1,7 @@
 class Trocla::Formats::Pgsql
   require 'digest/md5'
   def format(plain_password,options={})
-    raise "You need pass the username in the options for this format" unless options['username'] 
+    raise "You need pass the username as an option to use this format" unless options['username'] 
     "md5" + Digest::MD5.hexdigest(plain_password + options['username'])
   end
 end
