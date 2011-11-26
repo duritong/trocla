328b73bc759c3b8a1acd6923410911dfaef23822
mh <mh@immerda.ch>
2011-11-26 16:46:10 +0100

    rather use salt than random string to avoid not allowed characters

--- lib/trocla/formats/md5crypt.rb
+++ lib/trocla/formats/md5crypt.rb
@@ -1,6 +1,6 @@
 # salted crypt
 class Trocla::Formats::Md5crypt
   def format(plain_password,options={})
-     plain_password.crypt('$1$' << Trocla::Util.random_str(8) << '$')
+     plain_password.crypt('$1$' << Trocla::Util.salt << '$')
   end
 end
--- lib/trocla/formats/sha512crypt.rb
+++ lib/trocla/formats/sha512crypt.rb
@@ -1,6 +1,6 @@
 # salted crypt
 class Trocla::Formats::Sha512crypt
   def format(plain_password,options={})
-     plain_password.crypt('$6$' << Trocla::Util.random_str(8) << '$')
+     plain_password.crypt('$6$' << Trocla::Util.salt << '$')
   end
 end
