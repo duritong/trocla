9fa30f2acb34e60a4e9c4a4931cbf055daf9b31f
mh <mh@immerda.ch>
2013-08-20 16:57:40 +0200

    add ssha format

---
+++ lib/trocla/formats/ssha.rb
@@ -0,0 +1,9 @@
+# salted crypt
+require 'base64'
+require 'digest'
+class Trocla::Formats::Ssha
+  def format(plain_password,options={})
+     salt = options['salt'] || Trocla::Util.salt(16)
+     "{SSHA}"+Base64.encode64("#{Digest::SHA1.digest("#{plain_password}#{salt}")}#{salt}").chomp
+  end
+end
