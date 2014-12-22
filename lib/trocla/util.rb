8cbeeb7afb823daf88ce00cf021850e3be528cad
asquelt <asq@asq.art.pl>
2014-12-22 13:58:19 +0100

    not initialised @chars in shellsafe variant

--- lib/trocla/util.rb
+++ lib/trocla/util.rb
@@ -19,7 +19,7 @@ class Trocla
         @chars ||= normal_chars + special_chars
       end
       def safechars
-        @chars ||= normal_chars + shellsafe_chars
+        @shellsafe ||= normal_chars + shellsafe_chars
       end
       def normal_chars
         @normal_chars ||= ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
